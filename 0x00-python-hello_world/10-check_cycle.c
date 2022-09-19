4e484c7aabd7f4147d89522ee85e26222696c0df
fabrizia2 <melvinrenish56@gmail.com>
2022-09-19 15:08:18 -0700

    Correct output

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -3,15 +3,16 @@
 /**
  * check_cycle - checks if a singly linked list has a cycle in it
  *@list: singly list
+ *
  * Return: 0 if there is no cycle, 1 if there is a cycle
  */
 
 int check_cycle(listint_t *list)
 {
-	listint_t *link = list;
-	listint_t *last = list;
+	listint_t *link = list->next;
+	listint_t *last = list->next->next;
 
-	if (list == NULL)
+	if (list == NULL || list->next == NULL)
 		return (0);
 
 	while (link && last && last->next)
