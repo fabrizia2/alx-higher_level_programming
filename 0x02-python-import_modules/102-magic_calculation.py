a47e08c9343db8f4c4179dc4ad2b1308f7fa62b8
fabrizia2 <melvinrenish56@gmail.com>
2022-09-22 13:39:59 -0700

    Python function def magic_calculation(a, b): that does exactly the same as the following Python bytecode:

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    from mgic_calculation_102 import add, sub
+    if a < b:
+        summ = add(a, b)
+        for num in range(4, 6):
+            summ = add(c, num)
+        return (summ)
+    else:
+        return (sub(a, b))
