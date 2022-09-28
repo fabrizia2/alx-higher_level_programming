c646031bb39e53afbdf40db4da4d13cce82bb906
fabrizia2 <melvinrenish56@gmail.com>
2022-09-28 07:36:29 -0700

    a function that replaces or adds key/value in a dictionary.

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# @key argument will be always a string
+# @value argument will be any type
+# If a key exists in the dictionary, the value will be replaced
+# If a key doesn’t exist in the dictionary, it will be created
+
+def update_dictionary(a_dictionary, key, value):
+    for val in a_dictionary:
+        if val == key:
+            a_dictionary[key] = value
+    return a_dictionary
