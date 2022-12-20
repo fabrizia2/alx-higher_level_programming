657ada7741c6cd0ed0f9c337787406fe8fe1d29b
fabrizia2 <melvinrenish56@gmail.com>
2022-12-20 19:22:07 +0000

    fixing the error

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,8 +1,13 @@
 #!/usr/bin/node
+
+// Import Rectangle class from 4-rectangle.js
 const Rectangle = require('./4-rectangle');
 
-module.exports = class Square extends Rectangle {
+// Create a class Square that inherits from Rectangle
+class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
-};
+}
+
+module.exports = Square;
