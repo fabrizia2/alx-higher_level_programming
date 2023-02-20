bf7c6d4e8484b2ab8bb9db96b64b291d9eec9034
fabrizia2 <melvinrenish56@gmail.com>
2023-02-20 22:00:39 +0300

    JavaScript

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,4 @@
+document.querySelector('header').style.color = '#FF0000';
+/*
+ * This code first selects the <header> element using document.querySelector(), which searches the HTML document for the first matching element. It then sets the color property of the selected element's style object to the hex code for red (#FF0000), which changes the text color to red.
+ */
---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,4 @@
+$('header').css('color', '#FF0000');
+/*
+ * This code uses the jQuery function $() to select the <header> element, and then uses the css() method to update its text color to red (#FF0000).
+ */
